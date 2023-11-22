1dd02566a2e2117d4f334b33ab89f39ae9617141
Daniel Ramon <danielramoon@gmail.com>
2023-11-22 11:16:49 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-window">
+      <div className="sidebar">
+        <header>
+          <img
+            src="https://img.freepik.com/vetores-premium/avatar-icon002_750950-52.jpg"
+            alt="image"
+            className="header--avatar"
+          />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{ color: "#919191" }} />
+              <ChatIcon style={{ color: "#919191" }} />
+              <MoreVertIcon style={{ color: "#919191" }} />
+            </div>
+          </div>
+        </header>
+        <div className="search">...</div>
+        <div className="chatlist">...</div>
+      </div>
+      <div className="contentarea">...</div>
     </div>
   );
 }
